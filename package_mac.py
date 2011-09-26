54a66cf3fbb5934c6abc46f0a5308dd530f808c0
Stefan Richter <stefan@02strich.de>
2011-09-26 21:50:58 +0200

    Fix Mac package script

--- package_mac.py
+++ package_mac.py
@@ -1,8 +1,8 @@
 import os
 import zipfile
 
-myZipFile = zipfile.ZipFile("wymypy.wdgt", "w" )
+myZipFile = zipfile.ZipFile("wymypy.wdgt.zip", "w" )
 for root, dirs, files in os.walk("mac"):
     for filename in files:
         path = os.path.join(root, filename)
-        myZipFile.write(path, os.path.relpath(path, "mac"))
+        myZipFile.write(path, os.path.join("wymypy.wdgt", os.path.relpath(path, "mac")))
